281a1e2ea1859701d63243a81c79bd1039fecfe9
OscarMartinez5 <147806212+OscarMartinez5@users.noreply.github.com>
2023-10-13 10:05:28 +0200

    Add files via upload

---
+++ ejercicio1.java
@@ -0,0 +1,8 @@
+public class ejercicio1
+{
+    public static void main(String [] args)
+    {
+        System.out.println("Bienvenido a entornos de desarrollo donde vas a aprende a programar en java" );
+        System.out.println(3+4);
+    }
+}
